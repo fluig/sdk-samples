a06eee7bfd9d4888296ccd43a32c21635b2a14b2
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-23 16:06:47 -0300

    Create Android project

---
+++ android/Example/app/src/main/java/sdk/fluig/com/example/MainActivity.java
@@ -0,0 +1,13 @@
+package sdk.fluig.com.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
