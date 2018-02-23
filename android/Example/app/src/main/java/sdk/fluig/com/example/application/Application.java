eb47499e68f9703e533ad0768ed55a7c33f87897
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-23 16:25:05 -0300

    Add application level class to start SDK services

---
+++ android/Example/app/src/main/java/sdk/fluig/com/example/application/Application.java
@@ -0,0 +1,15 @@
+package sdk.fluig.com.example.application;
+
+import sdk.fluig.com.bll.core.base.view.application.FluigSdkApplication;
+
+/**
+ * Created by gregorysholl on 23/02/18.
+ */
+
+public class Application extends FluigSdkApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
