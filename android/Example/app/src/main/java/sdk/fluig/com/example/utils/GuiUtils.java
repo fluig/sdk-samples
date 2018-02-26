fbc444d5e5186cd9a0166fe1656b5bc90e913db1
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-26 11:20:23 -0300

    Create GuiUtils class to combine reusable interface codes

---
+++ android/Example/app/src/main/java/sdk/fluig/com/example/utils/GuiUtils.java
@@ -0,0 +1,33 @@
+package sdk.fluig.com.example.utils;
+
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by gregorysholl on 26/02/18.
+ */
+
+public class GuiUtils {
+
+    private GuiUtils() {}
+
+    public static void replaceFragment(AppCompatActivity activity,
+                                       @IdRes int containerViewId,
+                                       Fragment fragment,
+                                       String tag) {
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        if (fragmentManager.findFragmentByTag(tag) == null) {
+            fragmentManager.beginTransaction().replace(containerViewId, fragment, tag).commit();
+        }
+    }
+
+    public static void showToast(Context context, @StringRes int stringRes) {
+        Toast.makeText(context, context.getString(stringRes), Toast.LENGTH_SHORT).show();
+    }
+}
