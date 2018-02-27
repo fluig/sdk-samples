050b339a2d8b2f1afb58d5635b1fb5cec0aeca4f
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-27 17:19:20 -0300

    Add in GuiUtils a method to add fragments

--- android/Example/app/src/main/java/sdk/fluig/com/example/utils/GuiUtils.java
+++ android/Example/app/src/main/java/sdk/fluig/com/example/utils/GuiUtils.java
@@ -1,7 +1,11 @@
 package sdk.fluig.com.example.utils;
 
 import android.content.Context;
+import android.support.annotation.IdRes;
 import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 /**
@@ -14,5 +18,15 @@ public class GuiUtils {
 
     public static void showToast(Context context, @StringRes int stringRes) {
         Toast.makeText(context, context.getString(stringRes), Toast.LENGTH_SHORT).show();
+    }
+
+    public static void addFragment(AppCompatActivity activity,
+                                   @IdRes int containerViewId,
+                                   Fragment fragment,
+                                   String tag) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        if (fragmentManager.findFragmentByTag(tag) == null) {
+            fragmentManager.beginTransaction().add(containerViewId, fragment, tag).commit();
+        }
     }
 }
