1c0044e4fe23bbfa5d3fb3bd6ac62c5a4913f26f
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-27 15:54:11 -0300

    Create activity to display UI components

---
+++ android/Example/app/src/main/java/sdk/fluig/com/example/component/view/ComponentActivity.java
@@ -0,0 +1,15 @@
+package sdk.fluig.com.example.component.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sdk.fluig.com.example.R;
+
+public class ComponentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component);
+    }
+}
