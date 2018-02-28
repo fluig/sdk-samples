6152a12bb0710eff2ee0d99f1d777e99d0d486ed
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-02-28 10:59:16 -0300

    Add scroll view and proper text view to button fragment

--- android/Example/app/src/main/java/sdk/fluig/com/example/component/view/fragment/ButtonFragment.java
+++ android/Example/app/src/main/java/sdk/fluig/com/example/component/view/fragment/ButtonFragment.java
@@ -1,13 +1,16 @@
 package sdk.fluig.com.example.component.view.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sdk.fluig.com.example.R;
+import sdk.fluig.com.ui.components.button.FluigSdkButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +24,47 @@ public class ButtonFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+
+        setupView(view);
+
+        return view;
+    }
+
+    private void setupView(View view) {
+        if (view == null) {
+            return;
+        }
+
+        setupComponent(view);
+        setupTextView(view);
+    }
+
+    private void setupComponent(View view) {
+        FluigSdkButton button = view.findViewById(R.id.buttonFragment_component);
+
+        button.setText("OK");
+        button.setBackgroundColor(Color.BLUE);
+        button.setBackgroundColorEnd(Color.MAGENTA);
+        button.setCornerless(true);
+    }
+
+    private void setupTextView(View view) {
+        String text = "button.setText(\"OK\");\n" +
+                "button.setBackgroundColor(Color.BLUE);\n" +
+                "button.setBackgroundColorEnd(Color.MAGENTA);\n" +
+                "button.setCornerless(true);\n\n";
+
+        text = text +
+                "<sdk.fluig.com.ui.components.button.FluigSdkButton\n" +
+                "    android:layout_width=\"match_parent\"\n" +
+                "    android:layout_height=\"wrap_content\"\n" +
+                "    android:background=\"@color/blue\"\n" +
+                "    app:backgroundColor_end=\"@color/magenta\"\n" +
+                "    app:cornerless=\"true\" />";
+
+        TextView textView = view.findViewById(R.id.buttonFragment_textView);
+        textView.setText(text);
     }
 }
