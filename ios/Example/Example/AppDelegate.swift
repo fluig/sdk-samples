323ff3a9cfd14c7c456a4c4f7ca6bb42768e11e4
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-05 14:46:49 -0300

    Start the app at the main view controller

--- ios/Example/Example/AppDelegate.swift
+++ ios/Example/Example/AppDelegate.swift
@@ -13,8 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let main = MainViewController()
+        let navigationController = UINavigationController(rootViewController: main)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+        
         return true
     }
 }
