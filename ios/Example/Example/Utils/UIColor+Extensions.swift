2a7709f437fd1a0e644ffeb8e4f2185c25a65e61
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-05 14:01:09 -0300

    Add a color extension to put all app specific colors

---
+++ ios/Example/Example/Utils/UIColor+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Extensions.swift
+//  Example
+//
+//  Created by Gregory Sholl e Santos on 05/03/18.
+//  Copyright © 2018 fluig SDK. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    struct app {
+        static var blue: UIColor {
+            return UIColor(red: 63.0/255.0,
+                           green: 81.0/255.0,
+                           blue: 181.0/255.0,
+                           alpha: 1.0)
+        }
+    }
+}
