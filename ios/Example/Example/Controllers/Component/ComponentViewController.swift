519dd35c4dfbc2d05ebe6f493152f4b64ca23b0c
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-06 09:32:34 -0300

    Create protocol and inits for component view controller

--- ios/Example/Example/Controllers/Component/ComponentViewController.swift
+++ ios/Example/Example/Controllers/Component/ComponentViewController.swift
@@ -8,28 +8,34 @@
 
 import UIKit
 
+protocol ComponentViewDelegate {
+    
+    func showView(_ view: UIView)
+}
+
 class ComponentViewController: UIViewController {
 
+    private var componentType: ComponentType?
+    
+    init(componentType: ComponentType) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.componentType = componentType
+    }
+    
+    override init(nibName nibNameOrNil: String?,
+                  bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
