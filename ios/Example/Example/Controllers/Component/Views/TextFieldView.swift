c17aead691550930770a0d12ed4c470c0f29f02d
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-06 19:07:39 -0300

    Create layout for the text field view

--- ios/Example/Example/Controllers/Component/Views/TextFieldView.swift
+++ ios/Example/Example/Controllers/Component/Views/TextFieldView.swift
@@ -7,15 +7,65 @@
 //
 
 import UIKit
+import fluigSDKUi
 
 class TextFieldView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private weak var componentView: ComponentView?
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = .white
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let componentView = ComponentView(provider: self)
+        addSubview(componentView)
+        
+        NSLayoutConstraint.activate(
+            [componentView.topAnchor.constraint(equalTo: topAnchor),
+             componentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+             componentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+             componentView.bottomAnchor.constraint(equalTo: bottomAnchor)]
+        )
+        
+        self.componentView = componentView
+    }
+}
 
+extension TextFieldView: ComponentInfoProvider {
+    
+    func addComponent(to subview: inout UIView) {
+        let textField = FluigSDKTextField()
+        textField.placeholder = "Placeholder"
+        textField.icon = UIImage(named: "icnAccount")
+        textField.textFieldState = .valid
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        subview.addSubview(textField)
+        
+        NSLayoutConstraint.activate(
+            [textField.topAnchor.constraint(equalTo: subview.topAnchor),
+             textField.leadingAnchor.constraint(equalTo: subview.leadingAnchor),
+             textField.trailingAnchor.constraint(equalTo: subview.trailingAnchor),
+             textField.heightAnchor.constraint(equalToConstant: 50.0),
+             textField.bottomAnchor.constraint(equalTo: subview.bottomAnchor)]
+        )
+    }
+    
+    func provideComponentText() -> String {
+        return """
+        textField.placeholder = "Placeholder"
+        textField.icon = UIImage(named: "icnAccount")
+        textField.textFieldState = .valid
+        """
+    }
 }
