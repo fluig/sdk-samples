8d1382489a28df9af857d76c0aac4c2818d5e1f9
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-06 18:27:49 -0300

    Create a generic component view

---
+++ ios/Example/Example/Controllers/Component/Views/ComponentView.swift
@@ -0,0 +1,21 @@
+//
+//  ComponentView.swift
+//  Example
+//
+//  Created by Gregory Sholl e Santos on 06/03/18.
+//  Copyright © 2018 fluig SDK. All rights reserved.
+//
+
+import UIKit
+
+class ComponentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
