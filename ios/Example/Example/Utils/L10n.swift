56d2b75098262e4dfa5ac1e51e7515635ee41d04
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-07 11:06:25 -0300

    Move L10n's get method to a separate file

--- ios/Example/Example/Utils/L10n.swift
+++ ios/Example/Example/Utils/L10n.swift
@@ -47,14 +47,6 @@ extension L10n {
     let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
     return String(format: format, locale: Locale.current, arguments: args)
   }
-  
-  public static func get(_ key: String, _ args: CVarArg...) -> String {
-    let format = NSLocalizedString(key,
-                                   tableName: "Localizable",
-                                   bundle: Bundle(for: BundleToken.self),
-                                   comment: "")
-     return String(format: format, locale: Locale.current, arguments: args)
-  }
 }
 
 private final class BundleToken {}
