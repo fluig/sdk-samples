a2db7769ea77844a441c5a0d35178db15bdda4fd
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-07 11:14:58 -0300

    Readd the SwiftGen script to the project

--- ios/Example/Example/Utils/L10n.swift
+++ ios/Example/Example/Utils/L10n.swift
@@ -7,18 +7,6 @@ import Foundation
 
 // swiftlint:disable identifier_name line_length type_body_length
 enum L10n {
-  /// Error
-  static let error = L10n.tr("Localizable", "error")
-  /// Components
-  static let listComponents = L10n.tr("Localizable", "list_components")
-  /// Flows
-  static let listFlows = L10n.tr("Localizable", "list_flows")
-  /// The chosen mode is not implemented.
-  static let listNotFound = L10n.tr("Localizable", "list_notfound")
-  /// EULA
-  static let flowEula = L10n.tr("Localizable", "flow_eula")
-  /// Login
-  static let flowLogin = L10n.tr("Localizable", "flow_login")
   /// FluigSDKButton
   static let componentButton = L10n.tr("Localizable", "component_button")
   /// FluigSDKMediaView
@@ -27,17 +15,31 @@ enum L10n {
   static let componentTextField = L10n.tr("Localizable", "component_textField")
   /// FluigSDKTextFieldGroup
   static let componentTextFieldGroup = L10n.tr("Localizable", "component_textFieldGroup")
-  /// The chosen mode is not implemented.
-  static let loginTitle = L10n.tr("Localizable", "login_title")
-  /// The chosen mode is not implemented.
+  /// Error
+  static let error = L10n.tr("Localizable", "error")
+  /// EULA not accepted.
+  static let eulaNotAccepted = L10n.tr("Localizable", "eula_not_accepted")
+  /// EULA
+  static let flowEula = L10n.tr("Localizable", "flow_eula")
+  /// Login
+  static let flowLogin = L10n.tr("Localizable", "flow_login")
+  /// Components
+  static let listComponents = L10n.tr("Localizable", "list_components")
+  /// Flows
+  static let listFlows = L10n.tr("Localizable", "list_flows")
+  /// The chosen mode is not implemented
+  static let listNotFound = L10n.tr("Localizable", "list_not_found")
+  /// For more information, check the SDK documentation.
   static let loginTipInfo = L10n.tr("Localizable", "login_tip_info")
-  /// The chosen mode is not implemented.
+  /// http://tdn.totvs.com/display/SDKF/Login
   static let loginTipUrl = L10n.tr("Localizable", "login_tip_url")
+  /// Login flow example
+  static let loginTitle = L10n.tr("Localizable", "login_title")
   /// Success
   static let success = L10n.tr("Localizable", "success")
   /// You completed the flow %@.
-  static func successFlowSource(_ arg: String) -> String {
-    return L10n.tr("Localizable", "success_flow_source", arg)
+  static func successFlowSource(_ p1: String) -> String {
+    return L10n.tr("Localizable", "success_flow_source", p1)
   }
 }
 // swiftlint:enable identifier_name line_length type_body_length
