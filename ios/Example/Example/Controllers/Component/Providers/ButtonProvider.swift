5a7efce5114930490d36c323c33c3ea790f3f9f5
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-07 08:59:27 -0300

    Conform the created providers to the ComponentInfoProvider protocol

--- ios/Example/Example/Controllers/Component/Providers/ButtonProvider.swift
+++ ios/Example/Example/Controllers/Component/Providers/ButtonProvider.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2018 fluig SDK. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import fluigSDKUi
+
+struct ButtonProvider: ComponentInfoProvider {
+    
+    func provideComponent() -> UIView {
+        let button = FluigSDKButton()
+        button.setTitle("OK", for: .normal)
+        button.backgroundColor = .blue
+        button.backgroundColorEnd = .magenta
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+        
+        return button
+    }
+    
+    func provideComponentText() -> String {
+        return """
+        button.setTitle("OK", for: .normal)
+        button.backgroundColor = .blue
+        button.backgroundColorEnd = .magenta
+        """
+    }
+}
--- ios/Example/Example/Controllers/Component/Providers/TextFieldProvider.swift
+++ ios/Example/Example/Controllers/Component/Providers/TextFieldProvider.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2018 fluig SDK. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import fluigSDKUi
+
+struct TextFieldProvider: ComponentInfoProvider {
+    
+    func provideComponent() -> UIView {
+        let textField = FluigSDKTextField()
+        textField.placeholder = "Placeholder"
+        textField.icon = UIImage(named: "icnAccount")
+        textField.textFieldState = .valid
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.heightAnchor.constraint(equalToConstant: 50.0)
+            ])
+        
+        return textField
+    }
+    
+    func provideComponentText() -> String {
+        return """
+        textField.placeholder = "Placeholder"
+        textField.icon = UIImage(named: "icnAccount")
+        textField.textFieldState = .valid
+        """
+    }
+}
