c2a1122055d73b1a24edd85b9e897c7980fff7fd
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-03-12 09:50:45 -0300

    Add component implementation in profile pic provider

--- ios/Example/Example/Controllers/Component/Providers/ProfilePicProvider.swift
+++ ios/Example/Example/Controllers/Component/Providers/ProfilePicProvider.swift
@@ -12,14 +12,33 @@ import fluigSDKUi
 struct ProfilePicProvider: ComponentInfoProvider {
     
     func provideComponent() -> UIView {
-        let profilePic = FluigSDKProfilePic()
-        profilePic.image = nil
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
         
-        return profilePic
+        let profilePic = FluigSDKProfilePic()
+        profilePic.translatesAutoresizingMaskIntoConstraints = false
+        profilePic.image = UIImage(named: "doge.jpg")
+        profilePic.gradientColor = .black
+        profilePic.gradientColorEnd = .red
+        
+        container.addSubview(profilePic)
+        
+        NSLayoutConstraint.activate([
+            profilePic.topAnchor.constraint(equalTo: container.topAnchor),
+            profilePic.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+            profilePic.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            profilePic.widthAnchor.constraint(equalToConstant: 100.0),
+            profilePic.heightAnchor.constraint(equalToConstant: 100.0)
+        ])
+        
+        return container
     }
     
     func provideComponentText() -> String {
         return """
+        profilePic.image = UIImage(named: "doge.jpg")
+        profilePic.gradientColor = .black
+        profilePic.gradientColorEnd = .red
         """
     }
 }
